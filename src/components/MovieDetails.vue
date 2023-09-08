3b384e58ec0f1744a252c288afe0773c0ca3394e
Romaric97 <romaricmadegnan@gmail.com>
2023-09-08 18:49:37 +0100

    project initialization

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-if="movie" class="z-40 text-white pt-20 w-[50%]">
+    <h1 class="text-[55px] font-semibold font-serif">{{ movie.name }}</h1>
+    <div class="flex items-center text-lg pt-4">
+      <span>{{ movie.year }}</span>
+      <div class="relative">
+        <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+        <span class="pl-4">{{ movie.genre.one }}</span>
+      </div>
+      <div class="relative">
+        <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+        <span class="pl-4">{{ movie.genre.two }}</span>
+      </div>
+      <div class="relative">
+        <span class="absolute left-[2px] -top-[12px] text-[40px]">.</span>
+        <span class="pl-4">{{ movie.genre.three }}</span>
+      </div>
+    </div>
+    <p class="text-2xl pt-6">{{ movie.description }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  movie: Object,
+});
+</script>
